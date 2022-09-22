16c0d47f1195120adde191fd0b4d056237c35c85
1ucky40nc3 <louisnwendler@gmail.com>
2022-09-22 18:53:52 +0200

    parser arguments via argparse

--- src/train.py
+++ src/train.py
@@ -4,6 +4,8 @@ import torch
 
 import os
 import glob
+import argparse
+from typing import Optional
 
 import pytorch_lightning as pl
 
@@ -12,43 +14,167 @@ from models.vae import VqVaeModule
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-ROOT_DIR = os.getenv('ROOT_DIR', './lmd_full')
-OUTPUT_DIR = os.getenv('OUTPUT_DIR', './results')
-LOGGING_DIR = os.getenv('LOGGING_DIR', './logs')
-MAX_N_FILES = int(os.getenv('MAX_N_FILES', -1))
+def arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--root_dir',
+    type=str,
+    default='./lmd_full',
+    help='Dataset root dir.'
+  )
+  parser.add_argument(
+    '--output_dir',
+    type=str,
+    default='./results',
+    help='The output dir of a given run.'
+  )
+  parser.add_argument(
+    '--logging_dir',
+    type=str,
+    default='./logs',
+    help='Directory for logging files.'
+  )
+  parser.add_argument(
+    '--max_n_files',
+    type=int,
+    default=-1,
+    help='Number if MIDI files to process.'
+  )
+  parser.add_argument(
+    '--model',
+    type=Optional[str],
+    default=None,
+    help='Name of the model.'
+  )
+  parser.add_argument(
+    '--n_codes',
+    type=int,
+    default=2048,
+    help='Number of codes.'
+  )
+  parser.add_argument(
+    '--n_groups',
+    type=int,
+    default=16,
+    help='Number of groups.'
+  )
+  parser.add_argument(
+    '--d_model',
+    type=int,
+    default=512,
+    help='Model dim.'
+  )
+  parser.add_argument(
+    '--d_latent',
+    type=int,
+    default=1024,
+    help='Number of latents dims.'
+  )
+  parser.add_argument(
+    '--checkpoint',
+    type=Optional[str],
+    default=None,
+    help='Path to a pre-trained seq2seq checkpoint.'
+  )
+  parser.add_argument(
+    '--vae_checkpoint',
+    type=Optional[str],
+    default=None,
+    help='Path to a pre-trained vq-vae checkpoint.'
+  )
+  parser.add_argument(
+    '--batch_size',
+    type=int,
+    default=128,
+    help='Actual batch size on device.'
+  )
+  parser.add_argument(
+    '--target_batch_size',
+    type=int,
+    default=512,
+    help='Wanted batch size. Reached by accumulating batches.'
+  )
+  parser.add_argument(
+    '--epochs',
+    type=int,
+    default=16,
+    help='Number of training epochs.'
+  )
+  parser.add_argument(
+    '--warmup_steps',
+    type=int,
+    default=4000,
+    help='Number of warmup steps.'
+  )
+  parser.add_argument(
+    '--max_steps',
+    type=int,
+    default=1e20,
+    help='Maximum number of steps.'
+  )
+  parser.add_argument(
+    '--max_training_steps',
+    type=int,
+    default=100_000,
+    help='Maximum number of training steps.'
+  )
+  parser.add_argument(
+    '--learning_rate',
+    type=float,
+    default=1e-4,
+    help='Peak learning rate.'
+  )
+  parser.add_argument(
+    '--lr_schedule',
+    type=str,
+    default='const',
+    help='Type of the learning rate schedule.'
+  )
+  parser.add_argument(
+    '--context_size',
+    type=int,
+    default=256,
+    help='Size of sequence context.'
+  )
+  parser.add_argument(
+    '--precision',
+    type=int,
+    default=16,
+    help='Precision (16 or 32).'
+  )
+  parser.add_argument(
+    '--amp_backend',
+    type=str,
+    default='apex',
+    help='AMP backend for training with pytorch-lightning.'
+  )
+  parser.add_argument(
+    '--amp_level',
+    type=str,
+    default='apex',
+    help='AMP level for pl trainer.'
+  )
+  parser.add_argument(
+    '--n_workers',
+    type=Optional[int],
+    default=None,
+    help='Number of workers for the data pipeline.'
+  )
 
-MODEL = os.getenv('MODEL', None)
-MODEL_NAME = os.getenv('MODEL_NAME', None)
-N_CODES = int(os.getenv('N_CODES', 2048))
-N_GROUPS = int(os.getenv('N_GROUPS', 16))
-D_MODEL = int(os.getenv('D_MODEL', 512))
-D_LATENT = int(os.getenv('D_LATENT', 1024))
-
-CHECKPOINT = os.getenv('CHECKPOINT', None)
-VAE_CHECKPOINT = os.getenv('VAE_CHECKPOINT', None)
-
-BATCH_SIZE = int(os.getenv('BATCH_SIZE', 128))
-TARGET_BATCH_SIZE = int(os.getenv('TARGET_BATCH_SIZE', 512))
-
-EPOCHS = int(os.getenv('EPOCHS', '16'))
-WARMUP_STEPS = int(float(os.getenv('WARMUP_STEPS', 4000)))
-MAX_STEPS = int(float(os.getenv('MAX_STEPS', 1e20)))
-MAX_TRAINING_STEPS = int(float(os.getenv('MAX_TRAINING_STEPS', 100_000)))
-LEARNING_RATE = float(os.getenv('LEARNING_RATE', 1e-4))
-LR_SCHEDULE = os.getenv('LR_SCHEDULE', 'const')
-CONTEXT_SIZE = int(os.getenv('CONTEXT_SIZE', 256))
-
-ACCUMULATE_GRADS = max(1, TARGET_BATCH_SIZE//BATCH_SIZE)
-
-N_WORKERS = min(os.cpu_count(), float(os.getenv('N_WORKERS', 'inf')))
-if device.type == 'cuda':
-  N_WORKERS = min(N_WORKERS, 8*torch.cuda.device_count())
-N_WORKERS = int(N_WORKERS)
+  return parser.parse_args()
 
 
 def main():
-  ### Define available models ###
+  ### Prepare all arguments ###
+  args = arguments()
 
+  ACCUMULATE_GRADS = max(1, args.target_batch_size//args.batch_size)
+  N_WORKERS = min(os.cpu_count(), float(args.n_workers))
+  if device.type == 'cuda':
+    N_WORKERS = min(N_WORKERS, 8*torch.cuda.device_count())
+  N_WORKERS = int(N_WORKERS)
+
+  ### Define available models ###
   available_models = [
     'vq-vae',
     'figaro-learned',
@@ -63,24 +189,24 @@ def main():
     'baseline',
   ]
 
-  assert MODEL is not None, 'the MODEL needs to be specified'
-  assert MODEL in available_models, f'unknown MODEL: {MODEL}'
+  assert args.model is not None, 'the MODEL needs to be specified'
+  assert args.model in available_models, f'unknown MODEL: {args.model}'
 
 
   ### Create data loaders ###
-  midi_files = glob.glob(os.path.join(ROOT_DIR, '**/*.mid'), recursive=True)
-  if MAX_N_FILES > 0:
-    midi_files = midi_files[:MAX_N_FILES]
+  midi_files = glob.glob(os.path.join(args.root_dir, '**/*.mid'), recursive=True)
+  if args.max_n_files > 0:
+    midi_files = midi_files[:args.max_n_files]
 
   if len(midi_files) == 0:
-    print(f"WARNING: No MIDI files were found at '{ROOT_DIR}'. Did you download the dataset to the right location?")
+    print(f"WARNING: No MIDI files were found at '{args.root_dir}'. Did you download the dataset to the right location?")
     exit()
 
 
-  MAX_CONTEXT = min(1024, CONTEXT_SIZE)
+  MAX_CONTEXT = min(1024, args.context_size)
 
-  if MODEL in ['figaro-learned', 'figaro'] and VAE_CHECKPOINT:
-    vae_module = VqVaeModule.load_from_checkpoint(checkpoint_path=VAE_CHECKPOINT)
+  if args.model in ['figaro-learned', 'figaro'] and args.vae_checkpoint:
+    vae_module = VqVaeModule.load_from_checkpoint(checkpoint_path=args.vae_checkpoint)
     vae_module.cpu()
     vae_module.freeze()
     vae_module.eval()
@@ -93,7 +219,7 @@ def main():
 
   # load model from checkpoint if available
 
-  if CHECKPOINT:
+  if args.checkpoint:
     model_class = {
       'vq-vae': VqVaeModule,
       'figaro-learned': Seq2SeqModule,
@@ -106,8 +232,8 @@ def main():
       'figaro-no-chord': Seq2SeqModule,
       'figaro-no-meta': Seq2SeqModule,
       'baseline': Seq2SeqModule,
-    }[MODEL]
-    model = model_class.load_from_checkpoint(checkpoint_path=CHECKPOINT)
+    }[args.model]
+    model = model_class.load_from_checkpoint(checkpoint_path=args.checkpoint)
 
   else:
     seq2seq_kwargs = {
@@ -115,11 +241,11 @@ def main():
       'decoder_layers': 6,
       'num_attention_heads': 8,
       'intermediate_size': 2048,
-      'd_model': D_MODEL,
+      'd_model': args.d_model,
       'context_size': MAX_CONTEXT,
-      'lr': LEARNING_RATE,
-      'warmup_steps': WARMUP_STEPS,
-      'max_steps': MAX_STEPS,
+      'lr': args.learning_rate,
+      'warmup_steps': args.warmup_steps,
+      'max_steps': args.max_steps,
     }
     dec_kwargs = { **seq2seq_kwargs }
     dec_kwargs['encoder_layers'] = 0
@@ -131,15 +257,15 @@ def main():
         decoder_layers=6,
         encoder_ffn_dim=2048,
         decoder_ffn_dim=2048,
-        n_codes=N_CODES, 
-        n_groups=N_GROUPS, 
+        n_codes=args.n_codes, 
+        n_groups=args.n_groups, 
         context_size=MAX_CONTEXT,
-        lr=LEARNING_RATE,
-        lr_schedule=LR_SCHEDULE,
-        warmup_steps=WARMUP_STEPS,
-        max_steps=MAX_STEPS,
-        d_model=D_MODEL,
-        d_latent=D_LATENT,
+        lr=args.learning_rate,
+        lr_schedule=args.lr_schedule,
+        warmup_steps=args.warmup_steps,
+        max_steps=args.max_steps,
+        d_model=args.d_model,
+        d_latent=args.d_latent,
       ),
       'figaro-learned': lambda: Seq2SeqModule(
         description_flavor='latent',
@@ -178,19 +304,19 @@ def main():
         description_flavor='none',
         **dec_kwargs
       ),
-    }[MODEL]()
+    }[args.model]()
 
   datamodule = model.get_datamodule(
     midi_files,
     vae_module=vae_module,
-    batch_size=BATCH_SIZE, 
+    batch_size=args.batch_size, 
     num_workers=N_WORKERS, 
     pin_memory=True
   )
 
   checkpoint_callback = pl.callbacks.model_checkpoint.ModelCheckpoint(
     monitor='valid_loss',
-    dirpath=os.path.join(OUTPUT_DIR, MODEL),
+    dirpath=os.path.join(args.output_dir, args.model),
     filename='{step}-{valid_loss:.2f}',
     save_last=True,
     save_top_k=2,
@@ -204,10 +330,11 @@ def main():
     accelerator='dp',
     profiler='simple',
     callbacks=[checkpoint_callback, lr_monitor],
-    max_epochs=EPOCHS,
-    max_steps=MAX_TRAINING_STEPS,
+    max_epochs=args.epochs,
+    max_steps=args.max_training_steps,
     log_every_n_steps=max(100, min(25*ACCUMULATE_GRADS, 200)),
     val_check_interval=max(500, min(300*ACCUMULATE_GRADS, 1000)),
+    precision=args.precision,
     limit_val_batches=64,
     auto_scale_batch_size=False,
     auto_lr_find=False,
@@ -215,7 +342,7 @@ def main():
     stochastic_weight_avg=True,
     gradient_clip_val=1.0, 
     terminate_on_nan=True,
-    resume_from_checkpoint=CHECKPOINT
+    resume_from_checkpoint=args.checkpoint
   )
 
   trainer.fit(model, datamodule)
