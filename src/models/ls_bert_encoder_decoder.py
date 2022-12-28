d17cc81242acd6c0fee6b33e9597e8c61078f395
1ucky40nc3 <louisnwendler@gmail.com>
2022-12-28 23:56:58 +0100

    added hf style embedding interface & minor fixes

--- src/models/ls_bert_encoder_decoder.py
+++ src/models/ls_bert_encoder_decoder.py
@@ -21,9 +21,8 @@ from lightseq.training.ops.pytorch.quantization import (
     disable_quant,
     weight_quant_config,
     act_quant_config,
-    relu_quant_config,
 )
-from lightseq.training.ops.pytorch.torch_transformer_layers import BertEmbeddingLayer
+#from lightseq.training.ops.pytorch.torch_transformer_layers import BertEmbeddingLayer
 from transformers import (
     BertForSequenceClassification,
     BertPreTrainedModel,
@@ -35,6 +34,100 @@ from transformers import (
     BertForTokenClassification,
     BertForQuestionAnswering,
 )
+
+from lightseq.training.ops.pytorch import util
+from lightseq.training.ops.pytorch.layer_base import (
+    TransformerEmbeddingLayerBase,
+)
+
+def copy_para(x, fp16):
+    y = util.copy_para(x)
+    return y.half() if fp16 else y.float()
+
+from lightseq.training.ops.pytorch.quantization import (
+    TensorQuantizer,
+    act_quant_config,
+    weight_quant_config,
+    emb_quant_config,
+)
+
+from torch import nn
+
+
+class BertEmbeddingLayer(TransformerEmbeddingLayerBase):
+    def __init__(self, config, initial_weights=None):
+        super().__init__()
+        self.word_embeddings = nn.Embedding(
+            config.vocab_size, config.embedding_dim, padding_idx=config.padding_idx
+        )
+        self.position_embeddings = nn.Embedding(
+            config.max_seq_len, config.embedding_dim
+        )
+        self.token_type_embeddings = nn.Embedding(
+            config.type_vocab_size, config.embedding_dim
+        )
+
+        self.LayerNorm = nn.LayerNorm(config.embedding_dim, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.dropout)
+
+        self.register_buffer(
+            "position_ids", torch.arange(config.max_seq_len).expand((1, -1))
+        )
+        self.register_buffer(
+            "token_type_ids",
+            torch.zeros(self.position_ids.size(), dtype=torch.long),
+            persistent=False,
+        )
+
+        self.emb_quant = TensorQuantizer(emb_quant_config)
+
+        if initial_weights is None:
+            return
+
+        # load initial weights
+        self.word_embeddings.weight.data.copy_(
+            copy_para(initial_weights[0], config.fp16)
+        )
+        self.position_embeddings.weight.data.copy_(
+            copy_para(initial_weights[1], config.fp16)
+        )
+        self.token_type_embeddings.weight.data.copy_(
+            copy_para(initial_weights[2], config.fp16)
+        )
+        self.LayerNorm.weight.data.copy_(copy_para(initial_weights[3], config.fp16))
+        self.LayerNorm.bias.data.copy_(copy_para(initial_weights[4], config.fp16))
+
+    def forward(
+        self,
+        input_ids=None,
+        token_type_ids=None,
+        position_ids=None,
+        inputs_embeds=None,
+        past_key_values_length=0,
+    ):
+        # assert torch.all(token_type_ids == 0)
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+        seq_length = input_shape[1]
+        position_ids = self.position_ids[:, :seq_length]
+
+        token_type_ids = self.token_type_ids[:, :seq_length].expand(
+            input_shape[0], seq_length
+        )
+
+        if inputs_embeds is None:
+            inputs_embeds = self.word_embeddings(input_ids)
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+
+        embeddings = inputs_embeds + token_type_embeddings
+        embeddings = self.emb_quant(embeddings)
+        position_embeddings = self.position_embeddings(position_ids)
+        embeddings += position_embeddings
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
 
 
 def get_hf_bert_enc_layer_params(layer):
@@ -109,12 +202,12 @@ def inject_ls_layer(model, training_args, model_args, config):
 
     if model_args.module_type == 1 or model_args.module_type == 2:
         bert_emb_config = gen_bert_emb_config(training_args, config)
-        init_ws = get_hf_bert_emb_layer_params(model.bert.embeddings)
-        model.bert.embeddings = BertEmbeddingLayer(bert_emb_config, init_ws)
+        init_ws = get_hf_bert_emb_layer_params(model.embeddings)
+        model.embeddings = BertEmbeddingLayer(bert_emb_config, init_ws)
         if model_args.enable_quant:
-            model.bert.embeddings.apply(qat_mode)
+            model.embeddings.apply(qat_mode)
         else:
-            model.bert.embeddings.apply(disable_quant)
+            model.embeddings.apply(disable_quant)
 
     class LSHFTransformerEncoderLayer(TransformerEncoderLayer):
         def __init__(self, *args, **kwargs):
@@ -145,14 +238,14 @@ def inject_ls_layer(model, training_args, model_args, config):
 
     for i in range(config.num_hidden_layers):
         bert_enc_config = gen_bert_enc_config(training_args, config)
-        init_ws, init_bs = get_hf_bert_enc_layer_params(model.bert.encoder.layer[i])
-        model.bert.encoder.layer[i] = LSHFTransformerEncoderLayer(
+        init_ws, init_bs = get_hf_bert_enc_layer_params(model.encoder.layer[i])
+        model.encoder.layer[i] = LSHFTransformerEncoderLayer(
             bert_enc_config, init_ws, init_bs
         ).cuda()
         if model_args.enable_quant:
-            model.bert.encoder.layer[i].apply(enable_quant)
+            model.encoder.layer[i].apply(enable_quant)
         else:
-            model.bert.encoder.layer[i].apply(disable_quant)
+            model.encoder.layer[i].apply(disable_quant)
 
 
 def hf_state_dict(model):
@@ -199,16 +292,16 @@ def hf_state_dict(model):
     if not isinstance(model_to_save, LSBertPreTrainedModel):
         raise ValueError("Must be ligtseq replaced model")
     # reload original modules
-    ls_encoder_layer = model_to_save.bert.encoder.layer
-    model_to_save.bert.encoder.layer = torch.nn.ModuleList(
+    ls_encoder_layer = model_to_save.encoder.layer
+    model_to_save.encoder.layer = torch.nn.ModuleList(
         [BertLayer(model.config) for _ in range(model.config.num_hidden_layers)]
     )
     inject_hf_layer(
-        model_to_save.config, model_to_save.bert.encoder.layer, ls_encoder_layer
+        model_to_save.config, model_to_save.encoder.layer, ls_encoder_layer
     )
     state_dict = model_to_save.state_dict()
     # replace with lightseq modules
-    model_to_save.bert.encoder.layer = ls_encoder_layer
+    model_to_save.encoder.layer = ls_encoder_layer
     return state_dict
 
 
